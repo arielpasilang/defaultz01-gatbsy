1deeb56c988809084be74f4cbddae3591a606d08
defaultz12 <arielpasilang@gmail.com>
2018-11-03 23:40:59 +0800

    changed the col

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,8 @@ export default props =>(
 	<div>
 	<div className={styles.headertop}>
 		<div className="row">
-			<div className="col-6">0913258741</div>
-			<div className="col-6 text-right">arielpasilang@gmail.com</div>
+			<div className="col-5">0913258741</div>
+			<div className="col-7 text-right">arielpasilang@gmail.com</div>
 		</div>
 	</div>
 	<div className={styles.header}>
